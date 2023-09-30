9ac4b9dd8e810714b7d358c3bd7f63a413a1f2c3
nkumar007 <naveen.mbuff98@gmail.com>
2023-09-30 14:39:15 +0530

    add toolbox slice to the store

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
 import menuReducer from "@/slice/menuSlice";
+import toolboxReducer from "@/slice/toolboxSlice";
 
 const store = configureStore({
   reducer: {
     // Here we will be adding reducers
     menu: menuReducer,
+    toolbox: toolboxReducer,
   },
 });
 
