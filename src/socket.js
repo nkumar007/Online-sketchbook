a22f4d26712eb769e6c482b5d69c554742384ad0
nkumar007 <naveen.mbuff98@gmail.com>
2023-09-30 16:07:36 +0530

    update production server link

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,6 @@
 import {io} from "socket.io-client";
-
-const URL = "http://localhost:5000";
+const URL =
+  process.env.NODE_ENV === "production"
+    ? "https://sketchbook-server.onrender.com/"
+    : "http://localhost:5005";
 export const socket = io(URL);
